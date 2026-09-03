ed5f1bf387b057311bdc54ea07c3a3af8d167b6e
MTahir86 <gul.tahir123@gmail.com>
2026-09-03 18:47:50 +0500

    Set root directory in Vite config
    Forcefully set the root directory for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://js.org
 export default defineConfig({
   plugins: [react()],
+  root: './', // Forcefully setting root directory
   resolve: {
     alias: {
       '@': '/src',
