2a6d591dc199f6331b93a4fba9fe1ce2a01a4a34
MTahir86 <gul.tahir123@gmail.com>
2026-09-03 18:35:53 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://js.org
 export default defineConfig({
@@ -7,6 +8,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        // Agar index.html src folder ke andar hai to yeh line kaam karegi
+        main: resolve(__dirname, 'src/index.html'), 
+      },
     },
   },
 })
